994c8bab0259db5153ad437bd47f4cdddf6f906f
enzoguis <enzog0315@gmail.com>
2025-03-06 20:07:42 -0300

    fix(deploy): fixing the open ports detected deploy error

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import { app } from './app'
 import 'dotenv/config'
 import { env } from './models/env-schema'
 
-app.listen({ port: env.PORT }, () => {
+app.listen({ port: env.PORT, host: '0.0.0.0' }, () => {
   console.log('Server running on port: ', env.PORT)
 })
