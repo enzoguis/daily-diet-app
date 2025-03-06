c83cbc59d91b1dbaf0fd9907eee10debed3d2247
enzoguis <enzog0315@gmail.com>
2025-03-06 19:42:34 -0300

    feat(deploy): prepare to deploy

--- src/database.ts
+++ src/database.ts
@@ -2,9 +2,14 @@ import { Knex, knex as setupKnex } from 'knex'
 import 'dotenv/config'
 import { env } from './models/env-schema'
 
+const databaseConnection =
+  env.DATABASE_CLIENT === 'sqlite'
+    ? { filename: env.DATABASE_URL }
+    : env.DATABASE_URL
+
 export const config: Knex.Config = {
   client: env.DATABASE_CLIENT,
-  connection: env.DATABASE_URL,
+  connection: databaseConnection,
   useNullAsDefault: true,
   migrations: {
     extension: 'ts',
